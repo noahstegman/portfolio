c306a4fc889587f8e1153ff58d5ea0699baddf4b
stegmannoah@gmail.com <stegmannoah@gmail.com>
2023-01-31 07:59:07 -0800

    small updates

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,3 @@ $(window).on('load', function() {
     setTimeout(function() {
     }, 750)
   });
-
-
-$(".option").on({
-    mouseenter: function() {
-        $(this).find('div').text("> " + "Hello" + " <");
-    },
-    mouseleave: function() {
-        $(this).find('div').text("Hello")
-    }
-})
